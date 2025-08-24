6f05ed530d6278c61d20ce3cf4a93d6ef3e504d9
JahleelT <jlt9287@nyu.edu>
2025-08-24 15:58:05 -0400

    initialized repo, screens, and mock events

---
+++ components/EventCard.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function EventCard() {
+  return (
+    <View>
+      <Text>Event Card</Text>
+    </View>
+  );
+}
---
+++ components/RSVPButtons.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function RSVPButtons() {
+  return (
+    <View>
+      <Text>RSVP Buttons</Text>
+    </View>
+  );
+}
